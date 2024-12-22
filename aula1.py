0cc0a1c377a5dc79848e9add544f17c03e6a22a0
Snifx-255 <andrecarmo1234@gmail.com>
2024-12-22 19:01:50 -0300

    adiciona calculo de salario

---
+++ aula1.py
@@ -0,0 +1,15 @@
+'''
+# Variáveis
+velocidade_internet = 200
+print(velocidade_internet)
+# Números
+nota_filme = 8.5 # float, int,
+# Valores boleanos
+esta_aberto = True or False # condição de verdadeiro ou falso
+# Strings
+nome_do_curso = 'Lógica de Programação' # É texto
+'''
+salario = input("Digite o seu salário mensal")
+hora = input("Digite as suas horas trabalhadas por mês")
+valor_hora = int(salario) / int(hora)
+print(valor_hora)
